a602c6bf9f24ffcd3ae9c615f73501a65e0746c8
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-04-14 15:55:25 -0500

    add in error handlers, log handling, and routes

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const path = require('path');
+const cors = require('cors');
+const { logger, logEvents } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(logger);
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', require('./routes/root'));
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
