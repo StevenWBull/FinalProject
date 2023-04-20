c82a6e44a56eea45d5e1cc81b9b3e036433628f2
Steven Bull <51507134+StevenWBull@users.noreply.github.com>
2023-04-20 10:42:32 -0500

    update cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     'https://steven-bull-final-project.herokuapp.com/',
     'http://127.0.0.1:3000',
-    'http://localhost:3000'
+    'http://localhost:3000',
+    'https://dazzling-snickerdoodle-777101.netlify.app/'
 ];
 
 module.exports = allowedOrigins;
